64b59c7865bec841c40cc3ecd1a7c53ba4f4d9c1
Isadora Cândido <isa.candidos@gmail.com>
2024-11-09 12:47:32 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var elementosDuvida = document.querySelectorAll('.duvidas')
+
+elementosDuvida.forEach(function (duvidas) {
+     duvidas.addEventListener('click', function(){
+        duvidas.classList.toggle('ativa')
+     })   
+})
